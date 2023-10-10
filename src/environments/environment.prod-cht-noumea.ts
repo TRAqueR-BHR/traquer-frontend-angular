6e2d03b945d50532b6efc6f0ce2e6f697e99d88b
Vincent Laugier <>
2023-10-11 09:30:49 +1100

    bump

--- src/environments/environment.prod-cht-noumea.ts
+++ src/environments/environment.prod-cht-noumea.ts
@@ -13,7 +13,7 @@ export const environment = {
   numberOfSecondsBetweenChecksOfVersion: 120,
   numberOfSecondsBetweenCheckIfAnyPendingTask: 30,
   numberOfSecondsBetweenExecutionOfPendingTasks: 30,
-  frontEndVersion:"2023-09-17.02",
+  frontEndVersion:"2023-10-11.01",
   jwt_name: "traquer_jwt",
   cryptPwdHttpHeaderKey: "crypt_pwd",
   cryptPwdLocalStorageKey: "traquer_crypt_pwd",
