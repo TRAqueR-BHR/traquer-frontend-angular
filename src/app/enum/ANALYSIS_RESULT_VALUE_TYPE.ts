1a4cdb5777734b43b4f6e5d985c4f9ae58300f64
Vincent Laugier <vincent.laugier@gmail.com>
2023-10-16 21:04:13 +0000

    add suspicion for enums ANALYSIS_RESULT_VALUE_TYPE and INFECTIOUS_STATUS_TYPE

--- src/app/enum/ANALYSIS_RESULT_VALUE_TYPE.ts
+++ src/app/enum/ANALYSIS_RESULT_VALUE_TYPE.ts
@@ -2,5 +2,7 @@ export enum ANALYSIS_RESULT_VALUE_TYPE {
 
     positive = 1, 
     negative = 2, 
+    cancelled = 3, 
+    suspicion = 4, 
 
 }
--- src/app/enum/INFECTIOUS_STATUS_TYPE.ts
+++ src/app/enum/INFECTIOUS_STATUS_TYPE.ts
@@ -3,5 +3,6 @@ export enum INFECTIOUS_STATUS_TYPE {
     not_at_risk = 1, 
     carrier = 2, 
     contact = 3, 
+    suspicion = 4, 
 
 }
