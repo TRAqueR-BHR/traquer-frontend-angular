8b5fa545b7a99fbb377f79ad4fea4d46cb44ea8a
Vincent Laugier <vincent.laugier@gmail.com>
2023-11-13 06:34:03 +0000

    add attribute EventRequiringAttention.is_notification_sent

--- src/app/model/EventRequiringAttention.ts
+++ src/app/model/EventRequiringAttention.ts
@@ -1,20 +1,21 @@
 import { Appuser } from "src/app/module/appuser/model/Appuser";
 import { InfectiousStatus } from "./InfectiousStatus";
-import { EVENT_REQUIRING_ATTENTION_TYPE } from "../enum/EVENT_REQUIRING_ATTENTION_TYPE";
 import { RESPONSE_TYPE } from "../enum/RESPONSE_TYPE";
+import { EVENT_REQUIRING_ATTENTION_TYPE } from "../enum/EVENT_REQUIRING_ATTENTION_TYPE";
 
 export class EventRequiringAttention {
 
     responseUser:Appuser;
     infectiousStatus:InfectiousStatus;
     id:string;
+    refTime:Date;
+    responsesTypes:RESPONSE_TYPE[];
+    isPending:boolean;
+    eventType:EVENT_REQUIRING_ATTENTION_TYPE;
+    isNotificationSent:boolean;
     responseTime:Date;
     responseComment:string;
     creationTime:Date;
-    isPending:boolean;
-    eventType:EVENT_REQUIRING_ATTENTION_TYPE;
-    refTime:Date;
-    responsesTypes:RESPONSE_TYPE[];
 
     constructor(_json:Object) {
         if (_json['responseUser'] != null) {
@@ -24,29 +25,6 @@ export class EventRequiringAttention {
             this.infectiousStatus = new InfectiousStatus(_json['infectiousStatus']);
         }
         this.id = _json['id'];
-        if (_json['responseTime'] != null) {
-            if (_json['responseTime'] instanceof Date) {
-                this.responseTime = _json['responseTime'];
-            } else {
-                this.responseTime = new Date(_json['responseTime']);
-            }
-        }
-        this.responseComment = _json['responseComment'];
-        if (_json['creationTime'] != null) {
-            if (_json['creationTime'] instanceof Date) {
-                this.creationTime = _json['creationTime'];
-            } else {
-                this.creationTime = new Date(_json['creationTime']);
-            }
-        }
-        this.isPending = _json['isPending'];
-        if (_json['eventType'] != null) {
-            if (isNaN(Number(_json['eventType']))) {
-                this.eventType = Number(EVENT_REQUIRING_ATTENTION_TYPE[_json['eventType']]);
-            } else {
-                this.eventType = Number(_json['eventType']);
-            }
-        }
         if (_json['refTime'] != null) {
             if (_json['refTime'] instanceof Date) {
                 this.refTime = _json['refTime'];
@@ -62,6 +40,30 @@ export class EventRequiringAttention {
                 } else {
                     this.responsesTypes.push(Number(e));
                 }
+            }
+        }
+        this.isPending = _json['isPending'];
+        if (_json['eventType'] != null) {
+            if (isNaN(Number(_json['eventType']))) {
+                this.eventType = Number(EVENT_REQUIRING_ATTENTION_TYPE[_json['eventType']]);
+            } else {
+                this.eventType = Number(_json['eventType']);
+            }
+        }
+        this.isNotificationSent = _json['isNotificationSent'];
+        if (_json['responseTime'] != null) {
+            if (_json['responseTime'] instanceof Date) {
+                this.responseTime = _json['responseTime'];
+            } else {
+                this.responseTime = new Date(_json['responseTime']);
+            }
+        }
+        this.responseComment = _json['responseComment'];
+        if (_json['creationTime'] != null) {
+            if (_json['creationTime'] instanceof Date) {
+                this.creationTime = _json['creationTime'];
+            } else {
+                this.creationTime = new Date(_json['creationTime']);
             }
         }
     }
